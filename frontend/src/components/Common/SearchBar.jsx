9dfca10a226ea95a23fbec1bc531fb00e64ac5bb
ahasan-ullah <ahasanullah451@gmail.com>
2025-05-02 01:58:08 +0600

    handle search function added but not the functionality

--- frontend/src/components/Common/SearchBar.jsx
+++ frontend/src/components/Common/SearchBar.jsx
@@ -8,9 +8,12 @@ const SearchBar = () => {
   const handleSearchToggle=()=>{
     setIsOpen(!isOpen);
   }
+  const handleSearch=(e)=>{
+    e.preventDefault();
+  }
   return (
     <div className={`flex items-center justify-center w-full transition-all duration-300 ${isOpen?"absolute top-0 left-0 w-full bg-white h-24 z-50":"w-auto"}`}>
-      {isOpen?(<form className="relative flex items-center justify-center w-full">
+      {isOpen?(<form onSubmit={handleSearch} className="relative flex items-center justify-center w-full">
         <div className="relative w-1/2">
         <input onChange={(e)=>setSearchTerm(e.target.value)} type="text" placeholder="Search" value={searchTerm} className="bg-gray-100 px-4 py-2 pl-2 pr-12 rounded-lg focus:outline-none w-full placeholder:text-gray-700"/>
         {/* search icon */}
