915c6839a340085c0362d5e7498f13d44f3239a1
ahasan-ullah <ahasanullah451@gmail.com>
2025-05-02 12:48:36 +0600

    cart drawer designed but the cart content card are not added

--- frontend/src/components/Layout/CartDrawer.jsx
+++ frontend/src/components/Layout/CartDrawer.jsx
@@ -8,6 +8,17 @@ const CartDrawer = ({drawerOpen,toggleCartDrawer}) => {
       <div onClick={toggleCartDrawer} className="flex justify-end p-4">
         <button><IoMdClose className="h-6 w-6 text-gray-600"></IoMdClose></button>
       </div>
+      {/* Cart content */}
+      <div className="flex-grow p-4 overflow-y-auto">
+        <h2 className="text-xl font-semibold mb-4">Your Cart</h2>
+        {/* content card */}
+      </div>
+      {/* checkout button */}
+      <div className="p-4 bg-white sticky bottom-0">
+        <button className="w-full bg-black text-white py-3 rounded-lg font-semibold
+        hover:bg-gray-800 transition">Checkout</button>
+        <p className="text-sm tracking-tight text-gray-500 mt-2 text-center">Shipping, taxes and discount codes calculated at checkout.</p>
+      </div>
     </div>
   )
 }
