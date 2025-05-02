170506fcb65cc27eeac8cbeca0ab239f862499bc
ahasan-ullah <ahasanullah451@gmail.com>
2025-05-02 20:50:52 +0600

    hero section added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import UserLayout from './components/Layout/UserLayout';
+import Home from './pages/Home';
 const App = () => {
   return (
     <BrowserRouter>
     <Routes>
       {/* User Route */}
-      <Route path='/' element={<UserLayout/>}></Route>
+      <Route path='/' element={<UserLayout/>}>
+        <Route index element={<Home></Home>}></Route>
+      </Route>
       {/* Admin Route */}
       <Route></Route>
     </Routes>
