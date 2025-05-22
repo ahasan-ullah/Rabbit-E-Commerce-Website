3f1f33d07f43e1976e2348b59c30895a0f9565c2
ahasan-ullah <ahasanullah451@gmail.com>
2025-05-22 10:13:00 +0600

    product grid section modified so that the link can work

--- frontend/src/components/Products/ProductGrid.jsx
+++ frontend/src/components/Products/ProductGrid.jsx
@@ -1,25 +1,29 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-const ProductGrid = ({products}) => {
+const ProductGrid = ({ products }) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-      {
-        products.map((product,index) => (
-          <div key={index} className="p-4">
-            <Link key={index} to={`/product/${product._id}`}></Link>
+      {products.map((product, index) => (
+        <div key={index} className="p-4">
+          <Link key={index} to={`/product/${product._id}`}>
             <div className="bg-white p-4 rounded-lg">
               <div className="w-full h-96 mb-4">
-                <img src={product.images[0].url} alt={product.images[0].alt ||product.name} 
-                className="w-full h-full object-cover rounded-lg"/>
+                <img
+                  src={product.images[0].url}
+                  alt={product.images[0].alt || product.name}
+                  className="w-full h-full object-cover rounded-lg"
+                />
               </div>
               <h3 className="text-sm mb-2">{product.name}</h3>
-              <p className="text-gray-500 text-sm font-medium tracking-tighter">${product.price}</p>
+              <p className="text-gray-500 text-sm font-medium tracking-tighter">
+                ${product.price}
+              </p>
             </div>
-          </div>
-        ))
-      }
+          </Link>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default ProductGrid
+export default ProductGrid;
