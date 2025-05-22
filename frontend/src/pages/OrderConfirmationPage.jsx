2ce926790e2fc67a2b6d7b4137bc8aff3251e396
ahasan-ullah <ahasanullah451@gmail.com>
2025-05-22 23:54:49 +0600

    estimated delivery date added to the orderconfirmation page

--- frontend/src/pages/OrderConfirmationPage.jsx
+++ frontend/src/pages/OrderConfirmationPage.jsx
@@ -27,13 +27,18 @@ const checkout={
   }
 }
 const OrderConfirmationPage = () => {
+  const calculateEstimatedDelivery=(createdAt)=>{
+    const orderDate=new Date(createdAt);
+    orderDate.setDate(orderDate.getDate()+10);
+    return orderDate.toLocaleDateString();
+  }
   return (
     <div className="max-w-4xl mx-auto p-6 bg-white">
       <h1 className="text-4xl font-bold text-center text-emerald-700 mb-8">Thank you for the order!</h1>
 
       {
         checkout && (
-          <div className="p-6 rounded-lg border">
+          <div className="p-6 rounded-lg border border-gray-300">
             <div className="flex justify-between mb-20">
               {/* order id and date */}
               <div>
@@ -45,7 +50,14 @@ const OrderConfirmationPage = () => {
                 </p>
               </div>
               {/* estimated delivery */}
-              
+              <div>
+                <p className="text-emerald-700 text-sm">
+                  Estimated Delivery: {" "}
+                  {
+                    calculateEstimatedDelivery(checkout.createdAt)
+                  }
+                </p>
+              </div>
             </div>
           </div>
         )
