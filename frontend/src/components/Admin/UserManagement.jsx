77bfc313232628bfac31d08a811d606631f953f9
ahasan-ullah <ahasanullah451@gmail.com>
2025-05-23 13:11:44 +0600

    user list table added to the userManagement page

--- frontend/src/components/Admin/UserManagement.jsx
+++ frontend/src/components/Admin/UserManagement.jsx
@@ -8,6 +8,7 @@ const UserManagement = () => {
   })
   const users=[
     {
+      _id: 123,
       name:'John Doe',
       email:'john@example.com',
       role:'admin',
@@ -26,6 +27,16 @@ const UserManagement = () => {
       password:'',
       role:'customer',
     })
+  };
+
+  const handleRoleChange=(id,role)=>{
+    console.log({id,role});
+  };
+
+  const handleDeleteUser=(id)=>{
+    if(window.confirm("Are you sure you want to delete this user?")) {
+      console.log(`User with id ${id} deleted`);
+    }
   }
 
   return (
@@ -62,6 +73,38 @@ const UserManagement = () => {
           </div>
           <button type="submit" className="bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600 cursor-pointer">Add User</button>
         </form>
+      </div>
+      {/* user list */}
+      <div className="overflow-x-auto shadow-md sm:rounded-lg">
+        <table className="min-w-full text-left text-gray-500">
+          <thead className="bg-gray-100 text-xs uppercase text-gray-700">
+            <tr>
+              <th className="py-3 px-4">Name</th>
+              <th className="py-3 px-4">Email</th>
+              <th className="py-3 px-4">Role</th>
+              <th className="py-3 px-4">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              users.map(user=>(
+                <tr key={user._id} className="border-b hover:bg-gray-50">
+                  <td className="p-4 font-medium text-gray-900 whitespace-nowrap">{user.name}</td>
+                  <td className="p-4">{user.email}</td>
+                  <td className="p-4">
+                    <select value={user.role} onChange={(e)=>{handleRoleChange(user._id,e.target.value)}} className="p-2 border-gray-200 rounded">
+                      <option value="customer">Customer</option>
+                      <option value="admin">Admin</option>
+                    </select>
+                  </td>
+                  <td className="p-4">
+                    <button onClick={()=>{handleDeleteUser(user._id)}} className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 cursor-pointer">Delete</button>
+                  </td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
       </div>
     </div>
   )
