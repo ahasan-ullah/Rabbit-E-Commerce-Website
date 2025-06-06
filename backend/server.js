6dcac1049cc1a33feb85dc99a00c025cbbad2900
ahasan-ullah <ahasanullah451@gmail.com>
2025-06-06 22:12:28 +0600

    server is connected to the mongodb database

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const cors=require('cors');
 const dotenv=require('dotenv');
+const connectDB=require('./config/db');
 
 const app=express();
 app.use(express.json());
@@ -8,6 +9,8 @@ app.use(cors());
 dotenv.config();
 
 const PORT=process.env.PORT||3000;
+// connect to MongoDB
+connectDB();
 
 app.get('/',(req,res)=>{
   res.send('Welcome to the rabbit api!');
